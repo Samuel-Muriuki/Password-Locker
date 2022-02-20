1458db06380e574b2bab042184fe976dd01ec9f0
Samuel-Muriuki <samuel.muriuki@student.moringaschool.com>
2022-02-20 15:55:32 +0300

    Initializing and saving the user

--- pass_test.py
+++ pass_test.py
@@ -26,3 +26,21 @@ class TestUser(unittest.TestCase):
         """
         
         User.user_list = []
+        
+    def test_init(self): # Testing if objects have been inatantiated properly
+        """
+        Testing for proper object instantiation
+        """
+        
+        self.assertEqual(self.new_user.account_name, "Discord")
+        self.assertEqual(self.new_user.login_username, "bot")
+        self.assertEqual(self.new_user.password, "Lock-001")
+        
+    def test_save_user(self):
+        """
+        Testing if the user object is saved into the user list
+        """
+        
+        self.new_user.save_user()
+        self.assertEqual(len(User.user_list), 1)
+    
